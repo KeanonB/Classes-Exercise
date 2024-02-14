2452c0265937f1945c52aea2691115ea00f940af
Keanon Born <disposingmoon1@gmail.com>
2024-02-14 10:51:57 -0500

    done

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Classes
+﻿using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Classes
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car MyCar = new Car();
+            MyCar.Make = "Toyota";
+            MyCar.Model = "Takoma";
+            MyCar.Year = 2007;
+            Console.WriteLine($"{MyCar.Make},{MyCar.Model},{MyCar.Year}");
+
+            var Dodge = new Car()
+            {
+                Make = "Dodge Challenger",
+                Model = "Hellcat",
+                Year = 2015,
+            };
+            Console.WriteLine($"{Dodge.Make}, {Dodge.Model}, {Dodge.Year}");
+            
         }
+
+       
     }
 }
